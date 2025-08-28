d68e4c7a5f6c98278a8c834c8352fe37e78f99c2
Ofir <ofiraz757@gmail.com>
2025-08-28 22:28:57 +0300

    Delete PollSystemSprintBootProjects directory

--- PollSystemSprintBootProjects/PollsSystem/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO questions (title, option1, option2, option3, option4) VALUES
-('What do you most love to do?', 'Watch TV', 'Play the computer', 'Hanging out with friends', 'Travel the world'),
-('Where is your preferred place to travel?', 'USA', 'France', 'South America', 'Thailand');
